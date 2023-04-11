1df878e63dd48140a4023f08bf031cc18191a9c4
okylian <xenon@PC-KYLIAN>
2023-04-11 21:51:50 +0200

    display win message

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ let offsetX, offsetY;
 let gap = 50;
 
 function initializePieces() {
-  let row = 2;
-  let col = 2;
+  const row = 2;
+  const col = 2;
   const pieceWidth = image.width / row;
   const pieceHeight = image.height / col;
   for (let x = 0; x < row; x++) {
@@ -34,29 +34,37 @@ function initializePieces() {
       });
     }
   }
+  console.log(pieces);
 }
 
 
 
 function drawPieces() {
+  
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   
   pieces.forEach(function(piece) {
+
+      //dessine les bordures
+      ctx.lineWidth = 5;
+      ctx.strokeStyle = "black";
+      ctx.strokeRect(piece.imageX+piece.gap,piece.imageY+piece.gap, piece.width, piece.height);
+  });
+  
+  pieces.forEach(function(piece) {
     ctx.drawImage(image, piece.imageX, piece.imageY, piece.width, piece.height, piece.x, piece.y, piece.width, piece.height);
-    ctx.lineWidth = 2;
-  ctx.strokeStyle = "black";
-  ctx.strokeRect(piece.imageX+piece.gap,piece.imageY+piece.gap, piece.width, piece.height);
   });
 }
 
 
 function IsFinish(){
-  pieces.forEach(function(piece) {
-    if (piece.block === false) {
-      return drawPieces();
+  for (let i = 0; i < pieces.length; i++) {
+    if (pieces[i].block == false) {
+      return false;
     }
-    return true;
-  });
+  };
+  console.log(pieces, "finish")
+  return true;
 }
 
 canvas.addEventListener("mousedown", function(event) { //Fonction qui s'active quand on clique sur la piece 
@@ -69,6 +77,13 @@ canvas.addEventListener("mousedown", function(event) { //Fonction qui s'active q
       selectedPiece = piece;
       offsetX = mouseX - piece.x;
       offsetY = mouseY - piece.y;
+      //put selectedPiece a the end of the array pieces
+      if (selectedPiece.block == true) {
+        return;
+      }
+      pieces.splice(pieces.indexOf(selectedPiece), 1);
+      pieces.push(selectedPiece);
+      drawPieces();
     }
   });
 });
@@ -84,7 +99,6 @@ canvas.addEventListener("mousemove", function(event) { // lors du déplacement d
     const mouseY = event.clientY - canvas.offsetTop;
     selectedPiece.x = mouseX - offsetX;
     selectedPiece.y = mouseY - offsetY;
-    console.log(selectedPiece.block);
     drawPieces();
   }
 });
@@ -97,11 +111,15 @@ canvas.addEventListener("mouseup", function(event) {
     if (selectedPiece.x >= selectedPiece.imageX && selectedPiece.x < selectedPiece.imageX + selectedPiece.width && selectedPiece.y >= selectedPiece.imageY && selectedPiece.y < selectedPiece.height + selectedPiece.imageY) {
       selectedPiece.x = selectedPiece.imageX + selectedPiece.gap;
       selectedPiece.y = selectedPiece.imageY + selectedPiece.gap;
-      selectedPiece.block = true; 
-      if (IsFinish()) {
-        alert("Bravo");
-      }
+      selectedPiece.block = true;
+//put selectedPiece a the start of the array pieces
+      pieces.splice(pieces.indexOf(selectedPiece), 1);
+      pieces.unshift(selectedPiece);
     }
+  }
+  drawPieces();
+  if (IsFinish()) {
+    alert("Bravo !")
   }
   selectedPiece = null;
 });
